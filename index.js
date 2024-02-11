303921fa8329c76cece7b6dae7435efb2d5b4dd9
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-02-11 21:45:55 +0200

    finished main functionality

--- index.js
+++ index.js
@@ -24,7 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res) => {
+  const ip = req.ip;
+  const lang = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+
+  res.json({ipaddress: ip, language: lang, software: userAgent })
+})
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
